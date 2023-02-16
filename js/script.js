8cec97f25e5489a20c81c5a567c6afde1a2d5350
Maks <115740657+chuckchaa@users.noreply.github.com>
2023-02-16 15:44:12 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+
+gsap.registerPlugin(ScrollToPlugin);
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.querySelector('.menu-burger'),
+        scrollBtn = document.querySelector('.more'),
+        nav = document.querySelector('.nav'),
+        offsetY = getComputedStyle(nav).height.slice(0, -2);
+
+    const tl = gsap.timeline();
+
+    tl.from(".intro__title span", { y: 50, opacity: 0, stagger: .2 })
+        .from(".intro__text", { x: -50, opacity: 0 })
+        .from(".intro__button", { x: -20, opacity: 0 });
+
+    const titles = gsap.utils.toArray('.block-info__img');
+    console.log(titles);
+    titles.forEach(title => {
+        gsap.from(title, {
+            scrollTrigger: {
+                trigger: title,
+                start: "top center",
+            },
+            xPercent: -20,
+            opacity: 0,
+            duration: 1
+        });
+    });
+
+
+    menuBtn.addEventListener('click', (e) => {
+        document.documentElement.classList.toggle('menu-open');
+    });
+
+    scrollBtn.addEventListener('click', () => {
+        gsap.to(window, { duration: .8, scrollTo: { y: "#main", offsetY } });
+    });
+
+});
